f709cbb4821e8f47726ff6292c09e21fef8b3ef2
Thallyson Gabriel <thallyson.gabriel@discente.ufma.br>
2026-08-17 20:05:38 -0300

    007 - teste deploy 2

--- api/index.py
+++ api/index.py
@@ -2,10 +2,15 @@ import os
 import yaml
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+# Definindo o diretório base para localizar templates e dados corretamente no Vercel
+base_dir = os.path.dirname(os.path.abspath(__file__))
+template_dir = os.path.join(base_dir, '..', 'templates')
+static_dir = os.path.join(base_dir, '..', 'static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 def load_data():
-    data_path = os.path.join(os.path.dirname(__file__), '../data/resume.yaml')
+    data_path = os.path.join(base_dir, '..', 'data', 'resume.yaml')
     with open(data_path, 'r', encoding='utf-8') as file:
         return yaml.safe_load(file)
 
